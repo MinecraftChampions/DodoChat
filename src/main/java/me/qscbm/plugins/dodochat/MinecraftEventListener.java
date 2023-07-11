1a5b31c7d0f43a362352ddb201ff2767ec1543bd
qscbm187531 <1309635304@qq.com>
2023-07-11 17:30:04 +0800

    Update MinecraftEventListener.java

--- src/main/java/me/qscbm/plugins/dodochat/MinecraftEventListener.java
+++ src/main/java/me/qscbm/plugins/dodochat/MinecraftEventListener.java
@@ -2,6 +2,7 @@ package me.qscbm.plugins.dodochat;
 
 import com.velocitypowered.api.event.PostOrder;
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.connection.DisconnectEvent;
 import com.velocitypowered.api.event.player.PlayerChatEvent;
 import com.velocitypowered.api.event.player.ServerPostConnectEvent;
 import com.velocitypowered.api.proxy.Player;
@@ -12,6 +13,7 @@ import org.json.JSONObject;
 import java.io.IOException;
 
 public class MinecraftEventListener {
+    public static RegisteredServer lobbyServer = null;
     @Subscribe(order = PostOrder.LAST)
     public void onJoinServerEvent(ServerPostConnectEvent event) {
         if (!DodoChat.enableJoinMessage){
@@ -21,7 +23,6 @@ public class MinecraftEventListener {
         Player player = event.getPlayer();
         RegisteredServer server = player.getCurrentServer().get().getServer();
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("server",server.getServerInfo().getName());
         jsonObject.put("player", player.getUsername());
         String channelId = DodoChat.getConfiguration().getString("settings.Servers." + server.getServerInfo().getName());
         if (channelId.isEmpty()) {
@@ -33,8 +34,10 @@ public class MinecraftEventListener {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        if (lastSever != null || !DodoChat.enableLeaveMessage) {
-            jsonObject.put("lastServer",lastSever.getServerInfo().getName());
+        if (lastSever == null) {
+            lobbyServer = server;
+        }
+        if (lastSever != null && DodoChat.enableLeaveMessage) {
             String channelId1 = DodoChat.getConfiguration().getString("settings.Servers." + lastSever.getServerInfo().getName());
             if (channelId1.isEmpty()) {
                 DodoChat.getINSTANCE().getLogger().error("子服" + lastSever.getServerInfo().getName() + "没有指定Dodo频道");
@@ -45,6 +48,26 @@ public class MinecraftEventListener {
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
+        }
+    }
+
+    @Subscribe(order = PostOrder.LAST)
+    public void onChatEvent(DisconnectEvent event) {
+        if (!DodoChat.enableLeaveMessage) {
+            return;
+        }
+        Player player = event.getPlayer();
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("player", player.getUsername());
+        String channelId1 = DodoChat.getConfiguration().getString("settings.Servers." + player.getCurrentServer().get().getServerInfo().getName());
+        if (channelId1.isEmpty()) {
+            DodoChat.getINSTANCE().getLogger().error("子服" + player.getCurrentServer().get().getServerInfo().getName() + "没有指定Dodo频道");
+        }
+        String message = Utils.parsePlaceholders(DodoChat.getConfiguration().getString("settings.LeaveMessage.format"),jsonObject);
+        try {
+            ChannelMessageApi.sendTextMessage(DodoChat.authorization,channelId1,message);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
